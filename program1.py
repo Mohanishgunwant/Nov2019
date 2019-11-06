f23a8cdf5aa46b3a3848d98fee866618ad8312c9
Mohanish Gunwant <mohanish.gunwant@dal.ca>
2019-11-06 00:32:25 -0400

    program1

---
+++ program1.py
@@ -0,0 +1,10 @@
+#Python Program to Check if a Number is Positive, Negative or 0.
+
+num =  float(input("Enter the number :"))
+
+if num==0:
+    print("number is 0")
+elif num>0:
+    print("number is positive")
+else:
+    print("number is negative")
